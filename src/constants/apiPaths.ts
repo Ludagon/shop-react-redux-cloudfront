fb15693e0f5f3020490c4eb284a097a4ce5bc224
ldgonzalez26 <ldgonzalezmedina@gmail.com>
2024-03-30 21:13:46 -0500

    task-3
    task-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://api-managment-lg-service.azure-api.net/fa-products-service-lg-002",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
